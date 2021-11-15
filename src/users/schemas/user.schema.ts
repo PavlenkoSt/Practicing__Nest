bb031421e150e2c12b4fbedccc209ff71b15064d
Stanislav <verbatim8872@gmail.com>
2021-11-15 13:34:17 +0200

    add user schema

---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import UserRole from 'src/types/UserRole';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+  @Prop()
+  name: string;
+
+  @Prop()
+  password: number;
+
+  @Prop()
+  role: UserRole;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
