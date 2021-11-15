d4ae85cb54151b5326cce8a012aa72c5eea74159
Stanislav <verbatim8872@gmail.com>
2021-11-15 10:57:45 +0200

    add config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ConfigModule.forRoot()],
 })
 export class AppModule {}
