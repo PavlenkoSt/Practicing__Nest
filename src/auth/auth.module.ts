b918d345e045ca0767819b914ccdfc97ad75dc4d
Stanislav <verbatim8872@gmail.com>
2021-11-16 11:13:51 +0200

    token 1d

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -17,7 +17,7 @@ import { LocalStrategy } from './strategies/local.strategy';
     PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '30m' },
+      signOptions: { expiresIn: '1d' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy, RolesGuard],
